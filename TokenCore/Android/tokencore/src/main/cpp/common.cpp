7272f18ac65148c911657822d8a3224c7d8ab9ad
liangjisheng <1294851990@qq.com>
2019-02-14 19:30:38 +0800

    init commit

---
+++ TokenCore/Android/tokencore/src/main/cpp/common.cpp
@@ -0,0 +1,57 @@
+#include <jni.h>
+#include "../../../../../TokenCore/TokenCommon.h"
+
+using namespace std;
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_btxon_tokencore_Common__1gen_1seed(JNIEnv *env, jclass type, jint _size) {
+    int size = _size;
+    string result = gen_seed(size);
+    jstring ra = env->NewStringUTF(result.c_str());
+    return ra;
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_btxon_tokencore_Common__1entropy_1to_1mnemonic(JNIEnv *env, jclass type,
+                                                        jstring _entropy) {
+
+    const char *entropy = env->GetStringUTFChars(_entropy, 0);
+    string result = entropy_to_mnemonic(entropy);
+    jstring ra = env->NewStringUTF(result.c_str());
+    env->ReleaseStringUTFChars(_entropy, entropy);
+    return ra;
+}
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_btxon_tokencore_Common__1mnemonic_1to_1seed(JNIEnv *env, jclass type, jstring _mnemonic) {
+
+    const char *mnemonic = env->GetStringUTFChars(_mnemonic, 0);
+    string result = mnemonic_to_seed(mnemonic, "");
+    jstring ra = env->NewStringUTF(result.c_str());
+    env->ReleaseStringUTFChars(_mnemonic, mnemonic);
+    return ra;
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_btxon_tokencore_Common__1check_1mnemonic(JNIEnv *env, jclass type, jstring _mnemonic) {
+    const char *mnemonic = env->GetStringUTFChars(_mnemonic, 0);
+    bool result = check_mnemonic(mnemonic);
+    return result ? JNI_TRUE : JNI_FALSE;
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_btxon_tokencore_Common__1get_1sha(JNIEnv *env, jclass type, jstring _data) {
+    const char *data_r = env->GetStringUTFChars(_data, 0);
+    std::string pro_data_str = data_r;
+    std::string result = get_sha(pro_data_str);
+    jstring ra = env->NewStringUTF(result.c_str());
+    env->ReleaseStringUTFChars(_data, data_r);
+    return ra;
+}
