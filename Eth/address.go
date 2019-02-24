ed4da765a414800df93e9a48caf6931299f412ee
liangjisheng <1294851990@qq.com>
2019-02-24 16:49:08 +0800

    eth address

---
+++ Eth/address.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	genEthAddr()
+}
+
+func genEthAddr() {
+	key, _ := crypto.GenerateKey()
+
+	privateKey := hex.EncodeToString(key.D.Bytes())
+	address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+
+	fmt.Printf("privateKey: 0x%s\n", privateKey)
+	fmt.Printf("addr: %s\n", address)
+
+}
